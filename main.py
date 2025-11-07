f63cba580364f4f3b522157e78b3127c55fbfeeb
Kaew1 <16728@kalsbeek.nl>
2025-11-07 12:20:47 +0100

    kleur

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import pygame
 pygame.init()
 
 # Set up display
-width, height = 800, 600
+width, height = 800, 500
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Simple Pygame Window")
 clock = pygame.time.Clock()
 running = True
-bg_color = (153,211,232)
+bg_color = (153, 211, 232)
 font = pygame.font.SysFont(None, 48)
-text = font.render('Hello, Pygame!', True, (200, 200, 200))
+text = font.render('Chicken world', True, (83, 64, 175))
 text_rect = text.get_rect(center=(width // 2, height // 2))
 while running:
     for event in pygame.event.get():
@@ -21,3 +21,6 @@ while running:
     screen.blit(text, text_rect)
     pygame.display.flip()
     clock.tick(60)
+
+width, height = 800, 200
+color = (97, 215, 110)
