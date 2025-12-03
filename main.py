be72bcd40f75eabd859b9d7c897a74c7831eb306
Raff9 <16750@kalsbeek.nl>
2025-12-03 08:30:01 +0100

    geprobeerd inputs te maken

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import pygame
 
 pygame.init()
+color = (97, 215, 110)
+
+# Player properties
+player_width = 60
+player_height = 50
+player_x = 150
+player_y = 150
+player_x_direction = 0
+player_y_direction = 0
+player_speed = 5
+
+# Toetsen die ingedrukt worden
+keys = pygame.key.get_pressed()
+
+if keys[pygame.K_a]:
+    player_x -= player_speed
+if keys[pygame.K_d]:
+    player_x += player_speed
+if keys[pygame.K_w]:
+    player_y -= player_speed
+if keys[pygame.K_s]:
+    player_y += player_speed
 
 # Set up display
 width, height = 1000, 500
@@ -12,6 +34,10 @@ bg_color = (153, 211, 232)
 font = pygame.font.SysFont(None, 48)
 achtergrond = pygame.image.load('achtergrond 3.jpg')
 achtergrond = pygame.transform.scale(achtergrond, (width, height))
+player = pygame.image.load('Kip.png')
+player = pygame.transform.scale(player, (player_width, player_height))
+playerHitbox = player.get_rect()
+playerHitbox.topleft = (player_x, player_y)
 text = font.render('Chicken world', True, (83, 64, 175))
 text_rect = text.get_rect(center=(width // 2, height // 2))
 while running:
@@ -20,6 +46,8 @@ while running:
             running = False
 
     screen.blit(achtergrond, (0, 0))
+    pygame.draw.rect(screen,(100, 100, 100),(player_x, player_y, player_width, player_height))
+    screen.blit(player, (player_x, player_y))
 
     screen.blit(text, text_rect)
 
@@ -27,19 +55,9 @@ while running:
     pygame.display.flip()
     clock.tick(60)
 
-width, height = 800, 200
-color = (97, 215, 110)
 
-# Player properties
-player_width = 50
-player_height = 50
-player_x = 150
-player_y = 150
-player_x_direction = 0
-player_y_direction = 0
-player_speed = 5
 
 # Draw player
-pygame.draw.rect(screen, (255, 255, 0), (player_x, player_y, player_width, player_height))
+
 
 pygame.display.update()
