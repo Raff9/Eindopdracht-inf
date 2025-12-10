242331398c2979c608fb9c2619749056cad5fec2
Raff9 <16750@kalsbeek.nl>
2025-12-10 09:57:22 +0100

    hiitbox en ground gefixt

--- main.py
+++ main.py
@@ -25,16 +25,13 @@ achtergrond = pygame.image.load('achtergrond 3.jpg')
 achtergrond = pygame.transform.scale(achtergrond, (width, height))
 player = pygame.image.load('Kip.png')
 player = pygame.transform.scale(player, (player_width, player_height))
-playerHitbox = player.get_rect()
-playerHitbox.topleft = (player_x, player_y)
 text = font.render('Chicken world', True, (83, 64, 175))
 text_rect = text.get_rect(center=(width // 2, height // 2))
 
 # Hitbox van speler
 playerHitbox = player.get_rect()
 playerHitbox.width = 80
-playerHitbox.height = 90
-playerHitbox.topleft = (player_x, player_y)
+playerHitbox.height = 80
 
 while running:
     for event in pygame.event.get():
@@ -43,9 +40,9 @@ while running:
     
     old_x = player_x
     old_y = player_y
-    playerHitbox.topleft = (player_x, player_y)
+    playerHitbox.center = (player_x + player_width // 2, player_y + player_height // 2)
 
-    ground = pygame.Rect(650, 350, 100, 50)
+    ground = pygame.Rect(0, 450, 1000, 50)
 
     if playerHitbox.colliderect(ground):
         player_speed = 0.1
@@ -57,17 +54,18 @@ while running:
 
     if keys[pygame.K_a]:
         player_x -= player_speed
-    if keys[pygame.K_d] and not playerHitbox.colliderect(ground):
+    if keys[pygame.K_d]:
         player_x += player_speed
     if keys[pygame.K_w]:
         player_y -= player_speed
-    if keys[pygame.K_s]:
+    if keys[pygame.K_s] and not playerHitbox.colliderect(ground):
         player_y += player_speed
 
     screen.blit(achtergrond, (0, 0))
     screen.blit(player, (player_x, player_y))
     screen.blit(text, text_rect)
     pygame.draw.rect(screen, (139, 69, 19), ground)
+    pygame.draw.rect(screen, (255, 0, 0), playerHitbox, 2)
     pygame.display.flip()
 
     
